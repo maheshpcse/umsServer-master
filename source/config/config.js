47fe372c1320109df0ea2ba0dc533caf51d7ba11
Mahesh Pachapalam <maheshmahi1599@gmail.com>
2024-04-20 22:19:33 +0530

    Ignored env file and updated config.js file

--- source/config/config.js
+++ source/config/config.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 module.exports = {
     server: {
-        host: process.env.HOST,
-        port: process.env.PORT
+        host: process.env.HOST || 'localhost',
+        port: process.env.PORT || 80
     },
     database: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        db: process.env.DB_NAME,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        securitykey: process.env.SECURITY_KEY
+        host: process.env.DB_HOST || '69.57.172.68',
+        port: process.env.DB_PORT || 3306,
+        db: process.env.DB_NAME || 'meanapps_ums',
+        username: process.env.DB_USER || 'meanapps_adminmahesh',
+        password: process.env.DB_PASSWORD || 'Mysql@12345',
+        securitykey: process.env.SECURITY_KEY || 'UMSAPP'
     },
     email: {
         name: process.env.MAIL_NAME,
